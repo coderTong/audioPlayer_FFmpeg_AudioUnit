92ce69bc5b99a24037959f9cfdf8b1025553e2de
wutong <codewutong@foxmail.com>
2018-08-08 17:15:03 +0800

    init

---
+++ AudioUnitPlayerDemo/AudioUnitPlayerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AudioUnitPlayerDemo
+//
+//  Created by codew on 2018/8/8.
+//  Copyright © 2018年 codew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AudioUnitPlayerDemo/AudioUnitPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AudioUnitPlayerDemo
+//
+//  Created by codew on 2018/8/8.
+//  Copyright © 2018年 codew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
